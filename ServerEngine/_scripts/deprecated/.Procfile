d1ef9c15c5e4687a351bacaed849cc1e813f02ad
lmoresi <louis.moresi@gmail.com>
2016-01-30 21:48:11 +1100

    Um, forgot to add everything back after I moved it !

---
+++ ServerEngine/_scripts/deprecated/.Procfile
@@ -0,0 +1,3 @@
+jekyll:  jekyll build --watch
+compass: compass watch
+jupyter: jupyter notebook --NotebookApp.default_url="/files/index.html" --NotebookApp.file_to_run="_site/index.html"
---
+++ ServerEngine/config.rb
@@ -0,0 +1,12 @@
+# This is the compass config ...
+
+http_path = "/"
+css_dir = "_assets/stylesheets"
+sass_dir = "_assets/scss"
+images_dir = "_assets/images"
+javascripts_dir = "_assets/javascripts"
+
+output_style = :compressed
+
+# relative_assets = true
+line_comments = false
