fb3b41540a65f7feee8e044fca53fa75023af0c5
Louis Moresi <louis.moresi@unimelb.edu.au>
2016-02-23 21:46:39 +1100

    Well the mathjax is fixed now

--- docs/js/mathjax-local-config.js
+++ docs/js/mathjax-local-config.js
@@ -1,5 +1,18 @@
 /* Set local configuration for mathjax */
 
+/* Set local configuration for mathjax */
+
+MathJax.Hub.Config({
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX", "output/HTML-CSS"],
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+      processEscapes: true
+    },
+    "HTML-CSS": { availableFonts: ["TeX"] }
+  });
+
 /* Add line numbers by default */
 
 MathJax.Hub.Config({
