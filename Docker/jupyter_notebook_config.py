f0e55d198357d72e3924306d8ab5f658ed890b05
Louis Moresi <louis.moresi@gmail.com>
2019-03-12 10:25:22 +1100

    Removing spaces from all notebook filenames (jupyter lab hates this)

--- Docker/jupyter_notebook_config.py
+++ Docker/jupyter_notebook_config.py
@@ -1,8 +1,8 @@
 # Configuration file for jupyter-notebook.
 
 ## The default URL to redirect to from `/`
-## c.NotebookApp.default_url = '/tree/A0-Index.ipynb'
 
+c.NotebookApp.default_url = '/lab/tree/A0-Index.ipynb'
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
