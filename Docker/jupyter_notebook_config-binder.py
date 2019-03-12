8cbb1d9e253e93c970bb0eb3feba88eb2f58abe8
Louis Moresi <louis.moresi@gmail.com>
2019-03-12 12:59:36 +1100

    update with obspy and index.ipynb

--- Docker/jupyter_notebook_config-binder.py
+++ Docker/jupyter_notebook_config-binder.py
@@ -2,7 +2,7 @@
 
 ## The default URL to redirect to from `/`
 
-c.NotebookApp.default_url = 'lab/tree/0-StartHere.ipynb'
+c.NotebookApp.default_url = '/lab/tree/0-StartHere.ipynb'
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
