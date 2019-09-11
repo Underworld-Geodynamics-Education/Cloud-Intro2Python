f1b17f13d6c0b425601f0b43667c985e9d5c576a
lmoresi <lmoresi@users.noreply.github.com>
2019-09-11 16:41:58 +1000

    Restructure and Simplify

---
+++ CourseContent/uw-cloud/Docker/build-dockerfile.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Docker build command for the base python image
+# Currently we are at version 1.05
+
+# 1.05 includes the jupyter notebook extensions but not litho1pt0 and stripy
+
+#
+# set -e
+# cd $(dirname "$0")/.
+
+docker build -t lmoresi/docker-cloud-python:2019.09.11 -f Dockerfile  .
